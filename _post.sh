fbd35c1da028d44de9b9287f0b7b7303625ad231
Changmin Choi <cmchoi9901@gmail.com>
2020-03-02 20:05:44 +0900

    remove _post.sh

--- _post.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git subtree pull --prefix _site/ origin master
-bundle install
-JEKYLL_ENV=production bundle exec jekyll build
-git add .
-git commit
-git push
-git subtree push --prefix _site/ origin master
